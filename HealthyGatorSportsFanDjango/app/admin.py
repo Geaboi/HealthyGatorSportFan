02be0458316f8e7db74d048012b0f71ee150d897
Dustin Nguyen <dustinn1204@gmail.com>
2026-06-12 10:01:12 -0500

    Refactored Database to work for Garmin + Fitabase
    Removed FitBit OAuth. Added Stress with Garmin tables

--- HealthyGatorSportsFanDjango/app/admin.py
+++ HealthyGatorSportsFanDjango/app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
     User, UserData, NotificationData,
-    WearableDevice, HeartRateSample, ActivitySummary, EMA, JITAILog,
+    WearableDevice, HeartRateSample, StressSample, EMA, JITAILog,
 )
 
 admin.site.register(User)
@@ -9,6 +9,6 @@ admin.site.register(UserData)
 admin.site.register(NotificationData)
 admin.site.register(WearableDevice)
 admin.site.register(HeartRateSample)
-admin.site.register(ActivitySummary)
+admin.site.register(StressSample)
 admin.site.register(EMA)
 admin.site.register(JITAILog)
--- HealthyGatorSportsFanDjango/app/serializers.py
+++ HealthyGatorSportsFanDjango/app/serializers.py
@@ -1,23 +1,22 @@
 from rest_framework import serializers
-from .models import UserData, User, NotificationData, WearableDevice, HeartRateSample, ActivitySummary, EMA, JITAILog
+from .models import UserData, User, NotificationData, WearableDevice, HeartRateSample, StressSample, EMA, JITAILog
 from django.contrib.auth.hashers import make_password
 
 import logging
 
-# Serializes models to JSON for the front end
-# Deserializes and validates data from the front end, then saves it to the database
 
-# Best practice is one serializer per model
-
-# Serializer for User
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False, allow_blank=False, min_length=8)
 
     class Meta:
         model = User
-        fields = '__all__' #['user_id', 'email', 'password', 'first_name', 'last_name', 'birthdate', 'gender', 'height_feet', 'height_inches', 'goal_weight', 'goal_to_lose_weight', 'goal_to_feel_better']
-        # required fields in models.py, but these are overidden temporarily
+        fields = [
+            'user_id', 'email', 'password', 'first_name', 'last_name',
+            'birthdate', 'gender', 'height_feet', 'height_inches',
+            'goal_weight', 'goal_to_lose_weight', 'goal_to_feel_better',
+            'push_token', 'is_enrolled', 'enrolled_at',
+        ]
         read_only_fields = ('user_id',)
         extra_kwargs = {
             'birthdate': {'required': False, 'default': "2000-01-01"},
@@ -47,42 +46,13 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(pwd)
         instance.save()
         return instance
-    
-    # def create(self, validated_data):
-    #     return User.objects.create(
-    #         email=validated_data['email'],
-    #         password=validated_data['password'],
-    #         first_name = validated_data['first_name'],
-    #         last_name = validated_data['last_name'],
-    #         birthdate=validated_data['birthdate'],
-    #         gender=validated_data['gender'],
-    #         height_feet=validated_data['height_feet'],
-    #         height_inches=validated_data['height_inches'],
-    #         goal_weight=validated_data['goal_weight'],
-    #         goal_to_lose_weight=validated_data['goal_to_lose_weight'],
-    #         goal_to_feel_better=validated_data['goal_to_feel_better']
-    #     )
-    
-    # def update(self, instance, validated_data):
-    #     # Update fields when new data comes from the basicinfo.tsx screen
-    #     instance.first_name = validated_data.get('first_name', instance.first_name)
-    #     instance.last_name = validated_data.get('last_name', instance.last_name)
-    #     instance.birthdate = validated_data.get('birthdate', instance.birthdate)
-    #     instance.gender = validated_data.get('gender', instance.gender)
-    #     instance.height_feet = validated_data.get('height_feet', instance.height_feet)
-    #     instance.height_inches = validated_data.get('height_inches', instance.height_inches)
-    #     instance.goal_weight = validated_data.get('goal_weight', instance.goal_weight)
-    #     instance.goal_to_feel_better = validated_data.get('goal_to_feel_better', instance.goal_to_feel_better)
-    #     instance.goal_to_lose_weight = validated_data.get('goal_to_lose_weight', instance.goal_to_lose_weight)
-    #     instance.save()
-    #     return instance
 
-    
-# Serializer for UserData
+
 class UserDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserData
-        fields = ['data_id', 'user', 'timestamp', 'goal_type', 'weight_value', 'feel_better_value']        
+        fields = ['data_id', 'user', 'timestamp', 'goal_type', 'weight_value', 'feel_better_value']
+        read_only_fields = ('data_id', 'timestamp')
         extra_kwargs = {
             'goal_type': {'required': False},
             'weight_value': {'required': False, 'default': 0.0},
@@ -97,51 +67,51 @@ class UserDataSerializer(serializers.ModelSerializer):
         )
 
     def update(self, instance, validated_data):
-        # Update fields when new data comes from the basicinfo.tsx screen
         instance.goal_type = validated_data.get('goal_type', instance.goal_type)
         instance.weight_value = validated_data.get('weight_value', instance.weight_value)
         instance.feel_better_value = validated_data.get('feel_better_value', instance.feel_better_value)
         instance.save()
         return instance
-    
-# Serializer for NotificationData
+
+
 class NotificationDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationData
-        fields = '__all__'  # Or specify the fields you want to include
-        # fields = ['user', 'notification_title','notification_message', 'read_status']
+        fields = ['notification_id', 'user', 'notification_title', 'notification_message', 'timestamp', 'read_status']
+        read_only_fields = ('notification_id', 'timestamp')
 
 
 class WearableDeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = WearableDevice
-        fields = '__all__'
-        read_only_fields = ('device_id', 'created_at')
+        fields = ['id', 'user', 'fitabase_participant_id', 'device_name', 'is_active', 'last_synced_at']
+        read_only_fields = ('id',)
 
 
 class HeartRateSampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = HeartRateSample
-        fields = '__all__'
-        read_only_fields = ('sample_id',)
+        fields = ['id', 'user', 'timestamp', 'bpm', 'source']
+        read_only_fields = ('id',)
 
 
-class ActivitySummarySerializer(serializers.ModelSerializer):
+class StressSampleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ActivitySummary
-        fields = '__all__'
-        read_only_fields = ('summary_id',)
+        model = StressSample
+        fields = ['id', 'user', 'timestamp', 'stress_score', 'source']
+        read_only_fields = ('id',)
+
 
 
 class EMASerializer(serializers.ModelSerializer):
     class Meta:
         model = EMA
-        fields = '__all__'
-        read_only_fields = ('ema_id', 'timestamp')
+        fields = ['id', 'user', 'prompt_id', 'sent_at', 'responded_at', 'status', 'mood', 'stress', 'energy']
+        read_only_fields = ('id', 'sent_at')
 
 
 class JITAILogSerializer(serializers.ModelSerializer):
     class Meta:
         model = JITAILog
-        fields = '__all__'
-        read_only_fields = ('log_id', 'timestamp')
+        fields = ['id', 'user', 'prompt_id', 'triggered_at', 'trigger_reason', 'hr_at_trigger', 'stress_at_trigger', 'status']
+        read_only_fields = ('id', 'triggered_at')
